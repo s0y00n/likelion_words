a10a217969c38cc6c1ca2e101e37ed41f71bc294
s0y00n <leeesoyoon@gmail.com>
2020-05-26 18:49:37 +0900

    update

--- page/views.py
+++ page/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split() #문장을 나눠서 단어의 개수 세는 함수
+    word_dictionary = {}
+
+    for word in words:
+        if word in word_dictionary:
+            word_dictionary[word] +=1
+        else:
+            word_dictionary[word] =1
+    return render(request, 'result.html', {'full': text, 'total':len(words), 'dictionary':word_dictionary.items()})
